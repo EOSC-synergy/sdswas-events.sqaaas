97ca311ecb5e0e6474f1ba63beebc6d388c5704a
Marina Conde <marina.conde@bsc.es>
2021-03-17 20:11:00 +0100

    Solves issue #52 (Add search functionality to the Presentations section of a Past Event) but without loading animation yet and solves issue # 50 (Do not allow to open the full view of a Past Event Presentation from the presentations grid) but the preview link is still visible

--- src/sdswas/events/browser/static/js/event_presentations.js
+++ src/sdswas/events/browser/static/js/event_presentations.js
@@ -1,24 +1,16 @@
 var EventPresentations = {
 
     init: function(){
-        var url = $(".modwin-cards").attr("data-url");
-        $(".modwin-cards").load(url, function(responseTxt, statusTxt, xhr) {
-            if (statusTxt = "success") {
-                $(document).ready(function() {
-                    EventPresentations.cards.init();
-                });
-            } else $(this).text("Error: The contents are not available now. Please contact us.");
-        })
+        this.cardsContainer.init();
     },
 
     cards: {
-        selectors: $(".pastev-presentation-card"),
-        triggers: $(".js-card-trigger"),
         titles: $(".pastev-presentation-card-title  :first-child"),
         titleLenght: 135,
 
         init: function() {
             this.trimTextsLength();
+            this.addListeners();
         },
 
         trimTextsLength: function() {
@@ -31,5 +23,81 @@ var EventPresentations = {
 
             });
         },
+
+        addListeners: function() {
+
+            this.attachOpenItemView();
+
+            $(window).on("resize", function() {
+                App.winW = $(window).width();
+                App.winH = $(window).height();
+                Sidenav.repos();
+                EventPresentations.cards.attachOpenItemView();
+            });
+        },
+
+        attachOpenItemView: function(){
+            //Attachs to the on click event of children elements with class "js-card-trigger"
+            //the function that opens modal window. The click is unattached in screens < 592px
+
+            if (App.winW <= 592) {
+                $(".pastev-presentation-card.card-with-embedded-doc .js-card-trigger").each(function () {
+                   $(this).css("cursor", "default")
+                          .removeAttr("href");
+                });
+
+            } else {
+                $(".pastev-presentation-card.card-with-embedded-doc .js-card-trigger").each(function () {
+                    $(this).attr("href", $(this).attr("data-href"))
+                           .css("cursor", "pointer")
+                });
+
+            }
+        },
+
     },
+
+    cardsContainer: {
+        updateUrl: "",
+
+        init: function() {
+            App.con("----> Presentations cards container init");
+            this.addListeners();
+            this.updateUrl = $(".modwin-cards").attr("data-url");
+            this.update();
+        },
+
+        addListeners: function() {
+            App.con("----)))) Presentations of a Past Event >> Search input is listening");
+
+            $("form#searchPresentations").submit(function(e) {
+                e.preventDefault();
+                EventPresentations.cardsContainer.update();
+            });
+        },
+        update: function() {
+
+            //App.loader.init();
+
+            var searchinput = $("form#searchPresentations .search-tool-input").val()
+            var url = this.updateUrl+"?searchinput="+encodeURIComponent(searchinput)
+
+            $(".modwin-cards").empty().load(url, function(responseTxt, statusTxt, xhr) {
+                if (statusTxt == "success") {
+                    EventPresentations.cards.init();
+                } else {
+                    $(this).append("<p>Presentations can not be displayed now. Please <a href='contact-info'> contact us</a>.</p>");
+                }
+              /*  gsap.to(".page-overlay", {
+                        duration: 5,
+                        onComplete: function() {
+                            App.loader.end();
+                        }
+                });*/
+            })
+        },
+
+
+    }
+
 }
